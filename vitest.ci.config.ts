74883d1ad9880ac6c9a2e3377797744198a34a2d
TarouTanakaYokohama <haruto.916.041@gmail.com>
2025-05-28 21:56:37 +0900

    CIを追加

---
+++ vitest.ci.config.ts
@@ -0,0 +1,22 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vitest/config'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/e2e/**',
+      '**/.{idea,git,cache,output,temp}/**',
+    ],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+})
